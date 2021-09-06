f7532973140f708cbedcb9dd2f85df17025f3f77
Dunna14 <pedrobraz2002@gmail.com>
2021-09-06 23:44:38 +0100

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/Dunna14" target="_blank" rel="noreferrer">Pedro Braz</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+
+        </footer>
+
+    );
+}
+
+export default Footer;
